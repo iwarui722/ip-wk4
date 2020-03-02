f52df665f91227a40021b23f0cda7b8b2971e502
iwarui722 <warui.r.s@gmail.com>
2020-03-02 21:57:20 +0300

    add function

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('.summary').hide();
+    $('.cdata-overlay').hide();
+
+    $("#checkout").click(function () {
+        let flavour = $(".flavour option:selected").val();
+        let size = $("#size option:selected").val();
+        let crust = $("#crust option:selected").val();
+        let topping = $("#toppings option:selected").val();
+        let number = $("#number").val();
+        console.log(size);
+
